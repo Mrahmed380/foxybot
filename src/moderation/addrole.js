10f4776dc0356dbcfed656be459191d9866f6c0e
Natoons <67203817+Natoons@users.noreply.github.com>
2020-06-28 14:44:59 +0200

    Update addrole.js

--- src/moderation/addrole.js
+++ src/moderation/addrole.js
@@ -14,22 +14,22 @@ module.exports.run = async (bot, message, args) => {
     return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error** ');
 
   if (!message.member.hasPermission("MANAGE_ROLES"))
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to add roles.");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Vous ne disposez pas des autorisations suffisantes pour ajouter des rôles.");
 
   if (!message.guild.me.hasPermission("MANAGE_ROLES"))
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to manage roles.");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Je n'ai pas les autorisations suffisantes pour gérer les rôles.");
 
   if (!rMember)
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n " + "  Couldn't find that user");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n " + " Impossible de trouver cet utilisateur");
 
   if (!role)
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please supply a role");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Veuillez fournir un rôle");
 
   if (!gRole)
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " That role doesn't exist, if it does exist: check your spelling");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Ce rôle n'existe pas, s'il existe: vérifiez votre orthographe");
 
   if (rMember.roles.has(gRole.id))
-    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " This person already has that role.");
+    return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Cette personne a déjà ce rôle.");
 
   await (rMember.addRole(gRole.id));
 
